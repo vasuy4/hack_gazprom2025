553e65f6cea3cf06402755c2e8ef33839d0438e6
OwnerArtem <ar.ryqp@mail.ru>
2025-05-21 00:54:30 +0300

    base main lifespan

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
+import uvicorn
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # startup
+    yield
+    # shutdown
+
+main_app = FastAPI(lifespan=lifespan)
+
+
 if __name__ == '__main__':
-    pass
+    uvicorn.run(
+        "main:main_app"
+    )
